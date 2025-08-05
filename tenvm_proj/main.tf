846a9c356b1f420cbd30140293a3e938fd98bdd3
Kishoreus <137713866+Kishoreus@users.noreply.github.com>
2025-08-05 21:05:23 +0530

    Create main.tf

---
+++ tenvm_proj/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "default" {
+  key_name   = "mykeypair"
+  public_key = file("${path.module}/files/mykeypair.pub")
+}
+
+resource "aws_security_group" "vm_sg" {
+  name        = "vm_sg"
+  description = "Allow SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "vm" {
+  count         = 10
+  ami           = "ami-0fc5d935ebf8bc3bc" # UBUNTU
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.default.key_name
+  security_groups = [aws_security_group.vm_sg.name]
+
+  tags = {
+    Name = "vm-${count.index + 1}"
+  }
+}
