1252f216618a742e60d2b8e161e5e470f66b29e6
Kishoreus <137713866+Kishoreus@users.noreply.github.com>
2025-11-11 03:19:53 +0530

    Update main.tf

--- tenvm_proj/main.tf
+++ tenvm_proj/main.tf
@@ -8,13 +8,11 @@ resource "aws_key_pair" "default" {
 }
 
 terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "ec2/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks"
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
+
 
 resource "aws_security_group" "vm_sg" {
   name        = "vm_sg"
