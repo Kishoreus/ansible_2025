8be6532bedc80eef81fad8bfd3eb5d2501041305
Kishoreus <137713866+Kishoreus@users.noreply.github.com>
2025-11-11 03:15:58 +0530

    Update main.tf

--- tenvm_proj/main.tf
+++ tenvm_proj/main.tf
@@ -11,7 +11,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
     key            = "ec2/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-2"
     dynamodb_table = "terraform-locks"
   }
 }
